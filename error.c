2f6adf8736a570b2f965826e76ea9acef3a4702b
root <root@DESKTOP-FVV9G53>
2024-02-15 19:13:09 -0300

    the error arguments feature

---
+++ error.c
@@ -0,0 +1,26 @@
+
+#include "philo.h"
+
+static int  is_num(char *args);
+
+int ig_check_args(int argc, char **args)
+{
+	if (argc != 6 && argc != 5)
+		return(printf("ERROR: number of arguments\n"));
+	if (is_num(args[1]) || is_num(args[2]) || is_num(args[3])
+		|| is_num(args[4]) || (argc == 6 && is_num(args[5])))
+		return (printf("ERROR: arguments\n"));
+	if (ig_atoi(args[1]) < 0)
+		return (printf("ERROR: number of philosopher\n"));
+	if (ig_atoi(args[2]) < 0 || ig_atoi(args[3]) < 0
+		|| ig_atoi(args[4]) < 0 || (argc == 6 && ig_atoi(args[5]) < 0))
+		return (printf("ERROR: number of times\n"));
+	return (0);
+}
+
+static int  is_num(char *args)
+{
+	while((*args >= '0' && *args <= '9') || (*args == '-' || *args == '+'))
+		args++;
+	return (*args);
+}
