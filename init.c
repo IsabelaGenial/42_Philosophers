86e9ea7b861af240e2cfca6114e9d468b04e89ec
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-02-16 11:57:49 -0300

    fix: type malloc and reference in structure

--- init.c
+++ init.c
@@ -1,26 +1,25 @@
 
 #include "philo.h"
 
-t_main *init_dinner(int argc, char **args)
+t_main *ig_init_dinner(int argc, char **args)
 {
-	t_main  dining_philos;
-	int     i;
+	t_main  *dining_philos;
 
-	dining_philos = malloc(sizeof(t_main));
-	dining_philos.info = malloc(sizeof(t_inf));
-	dining_philos.info.nu_philos = ig_atoi(args[1]);
-	dining_philos.info.time_to_die = ig_atoi(args[2]);
-	dining_philos.info.time_to_eat = ig_atoi(args[3]);
-	dining_philos.info.time_to_sleep = ig_atoi(args[4]);
-	dining_philos.info.times_each_philosopher_eat = -1;
+	dining_philos = (t_main*)malloc(sizeof(t_main));
+	dining_philos->info = (t_inf*)malloc(sizeof(t_inf));
+	dining_philos->info->nu_philos = ig_atoi(args[1]);
+	dining_philos->info->time_to_die = ig_atoi(args[2]);
+	dining_philos->info->time_to_eat = ig_atoi(args[3]);
+	dining_philos->info->time_to_sleep = ig_atoi(args[4]);
+	dining_philos->info->each_philos_eat = -1;
 	if (argc == 6)
-		dining_philos.info.each_philos_eat = ig_atoi(args[5]);
-	dining_philos.dead_philo = 0;
-	dining_philos.philo = malloc(ig_atoi(args[1]) * sizeof(t_philo));
-	pthread_mutex_init(&dining_philos.mx_each_ate_enough, NULL);
-	pthread_mutex_init(&dining_philos.mx_phlio_state, NULL);
-	pthread_mutex_init(&dining_philos.mx_dead_philo, NULL);
-	pthread_mutex_init(&dining_philos.mx_print, NULL);
+		dining_philos->info->each_philos_eat = ig_atoi(args[5]);
+	dining_philos->dead_philo = 0;
+	dining_philos->philo = (t_philo *)malloc(ig_atoi(args[1]) * sizeof(t_philo));
+	pthread_mutex_init(&dining_philos->mx_each_ate_enough, NULL);
+	pthread_mutex_init(&dining_philos->mx_phlio_state, NULL);
+	pthread_mutex_init(&dining_philos->mx_dead_philo, NULL);
+	pthread_mutex_init(&dining_philos->mx_print, NULL);
 	return (dining_philos);
 }
 
@@ -37,6 +36,6 @@ int ig_atoi(const char *string)
 			if (*string++ == '-')
 				neg = -1;
 	while (*string >= '0' && *string <='9')
-		numb = (n * 10) + ((*string++) - 48);
-	return (numb * neg)
+		numb = (numb * 10) + ((*string++) - 48);
+	return (numb * neg);
 }
