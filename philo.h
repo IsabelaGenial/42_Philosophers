01118ba517b0843343554319ce6e281b50d1fe62
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-02-21 19:43:09 -0300

    feat: create thread

--- philo.h
+++ philo.h
@@ -59,6 +59,7 @@ typedef struct s_main
 {
 	t_inf					*info;
 	t_philo					*philo;
+	t_philo_state			state;
 	int						dead_philo;
 	pthread_mutex_t			*forks;
 	pthread_mutex_t			mx_phlio_state;
@@ -77,7 +78,9 @@ t_thread	*ig_init_thread_table(t_main *dining);
 t_main		*ig_init_dinner(int argc, char **args);
 void		*ig_philo_thread(void *param);
 void 		ig_threads(t_main *dinner);
+void		ig_check_forks(t_thread *dinner, int captor, int both );
 int     	ig_atoi(const char *string);
 int    		ig_check_args(int argc, char **args);
 int			ig_check_thread(t_thread *dinner, int closer);
+void		ig_state(t_thread *dinner, enum e_philo_state mode);
 #endif
