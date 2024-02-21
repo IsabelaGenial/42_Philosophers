722449fb0f57d38f1af831820fb6e9c0993508f6
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-02-21 19:40:32 -0300

    feat: check forks and print state

--- check.c
+++ check.c
@@ -20,3 +20,30 @@ int ig_check_thread(t_thread *dinner, int closer)
 	pthread_mutex_unlock(&dinner->dinning.mx_dead_philo);
 	return (0);
 }
+
+void ig_state(t_thread *dinner, enum e_philo_state mode)
+{
+	struct timeval time;
+	long get_time;
+
+	ig_check_thread(dinner, 1);
+	gettimeofday(&time, NULL);
+	get_time = ((time.tv_sec * 1000L) + (time.tv_usec / 1000L));
+	dinner->dinning.philo[dinner->i].state = (t_philo_state){mode, get_time};
+	pthread_mutex_lock(&dinner->dinning.mx_print);
+	printf("%ld %d %s\n", get_time, dinner->i + 1,
+		   (char [5][20]){"has taken a fork", "is eating", "is sleeping",
+			"is thinking", "died"}[mode]);
+	pthread_mutex_unlock(&dinner->dinning.mx_print);
+}
+
+void ig_check_forks(t_thread *dinner, int captor, int both )
+{
+	if(ig_check_thread(dinner, 0))
+	{
+		pthread_mutex_unlock(&dinner->dinning.forks[captor]);
+		if (both)
+			pthread_mutex_unlock(&dinner->dinning.forks[both]);
+		exit(0);
+	}
+}
