59fffcf946acf5041e9cb293de61560f7c7f646b
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-02-21 19:43:00 -0300

    feat: create thread

--- main.c
+++ main.c
@@ -14,8 +14,9 @@
 
 int	main(int argc, char **args)
 {
-	t_main *dining_philos;
-	t_thread *dinner;
+	t_main		*dining_philos;
+	t_thread 	*dinner;
+	int 		i;
 
 	if(ig_check_args(argc, args))
 		return (0);
@@ -24,7 +25,9 @@ int	main(int argc, char **args)
 		return(printf("error malloc"));
 	dinner = ig_init_thread_table(dining_philos);
 	ig_threads(&dinner->dinning);
-
-
+	i = 0;
+	while (i < dinner->dinning.info->nu_philos)
+		pthread_join(*dinner->dinning.philo[i++].pthread, NULL);
+	free(dinner);
 	return(0);
 }
